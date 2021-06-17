b01d1a187fef8ddb3909b9dc3748af8a5f0a88dd
danielabba <nieljames.jr@gmail.com>
2021-06-17 11:49:59 -0700

    This is the first part

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Return Always 0 (Success)
+*/
+
+int main(void)
+{
+    char acharacter;
+    int ainteger;
+    long along;
+    long long alonglong;
+    float afloat;
+
+    printf("Size of a char: %lu byte(s)\n", sizeof(acharacter))
